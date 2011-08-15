24829f3f671861798ee0b7cb0dcc805577c5f880
Luke Redpath <luke@lukeredpath.co.uk>
2011-08-16 00:35:10 +0100

    Remove TouchJSON as it's included in libPusher.

---
+++ PusherChat-iPhone/PusherChatMonitor.h
@@ -0,0 +1,13 @@
+//
+//  PusherChatMonitor.h
+//  PusherChat-iPhone
+//
+//  Created by Luke Redpath on 16/08/2011.
+//  Copyright 2011 LJR Software Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PusherChatMonitor : NSObject
+
+@end
---
+++ PusherChat-iPhone/PusherChatMonitor.m
@@ -0,0 +1,23 @@
+//
+//  PusherChatMonitor.m
+//  PusherChat-iPhone
+//
+//  Created by Luke Redpath on 16/08/2011.
+//  Copyright 2011 LJR Software Limited. All rights reserved.
+//
+
+#import "PusherChatMonitor.h"
+
+@implementation PusherChatMonitor
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
